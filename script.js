d27628b1034f0a34a4e3717fb5a39922b1f36e94
AnaghaPatil189 <109735323+AnaghaPatil189@users.noreply.github.com>
2022-09-03 14:31:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    let bulbobj=document.querySelector(".bulboff")
+    bulbobj.setAttribute("src","https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg")
+}
+
+function bulbOff(){
+    let bulbobj=document.querySelector(".bulboff")
+    bulbobj.setAttribute("src","https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png")
+}
